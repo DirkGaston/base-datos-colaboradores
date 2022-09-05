e706178e58c0036d859271ce2ef68fccafa18be5
DirkGaston <dirkgaston87@gmail.com>
2022-09-05 14:36:59 -0300

    search function corrected

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 const Form = ({ createCollab }) => {
   const [collabName, setCollabName] = useState("");
-  const [collabEmail, setCollabEmail] = useState("");
+  const [collabMail, setCollabMail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createCollab(collabName, collabEmail);
-    setNombre("");
-    setCorreo("");
+    createCollab(collabName, collabMail);
+    setCollabName("");
+    setCollabMail("");
   };
   return (
     <div className="inputContainer ml-6 mr-6">
@@ -44,7 +44,7 @@ const Form = ({ createCollab }) => {
             className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Ingresa correo del colaborador"
             onChange={(e) => {
-              setCollabEmail(e.target.value);
+              setCollabMail(e.target.value);
             }}
             required
           />
